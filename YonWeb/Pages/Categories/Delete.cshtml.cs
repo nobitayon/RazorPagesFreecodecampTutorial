7126d4309a6727e300e12f9ec2b2729e48249c7a
nobitayon <yonrungrung@gmail.com>
2024-07-03 20:30:19 +0700

    Delete category
    - but not utilized ModelState

---
+++ YonWeb/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using YonWeb.Data;
+using YonWeb.Model;
+
+namespace YonWeb.Pages.Categories;
+
+public class DeleteModel : PageModel
+{
+    private readonly ApplicationDbContext _db;
+
+    [BindProperty]
+    public Category Category { get; set; }
+
+    public DeleteModel(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    
+
+    public void OnGet(int id)
+    {
+        Category = _db.Category.Find(id);
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        var categoryFromDb = _db.Category.Find(Category.Id);
+        if (categoryFromDb != null)
+        {
+            _db.Category.Remove(categoryFromDb);
+            await _db.SaveChangesAsync();
+            return RedirectToPage("Index");
+        }
+        
+        return Page();
+    }
+}
